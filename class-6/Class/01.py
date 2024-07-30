e51e7656c441494d29bdca7c69622bd2444e4323
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-07-30 18:36:56 +0600

    code

---
+++ class-6/Class/01.py
@@ -0,0 +1,10 @@
+# use of if else
+
+a=5
+b=10
+compare= a>b
+if compare==True:
+    print(" the compare is true  ") # then execute if
+else:
+    print(" if the compare is false ") # ,then execute else
+    
