28fed28ad9cc9f8fa3b195d0a310087d81787f19
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-08-03 22:48:52 +0600

    code

---
+++ class-7/Homework/Medium/Medium_1.py
@@ -0,0 +1,12 @@
+"""
+[*] Multiplication Table:
+    Write a program that prints the multiplication table of a number entered by the user using a for loop.
+"""
+
+
+n = int(input(" Enter a number : "))
+multiplication = 0
+print(f" {n}th multiplication table :")
+for i in range(1,11):
+    multiplication = n * i
+    print(f"  {n} * {i} = {multiplication} ")
