f5c022d999e24ca2a17e5c4ba111f6cae6363d32
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-08-03 22:26:45 +0600

    code

--- class-6/Class/01.py
+++ class-6/Class/01.py
@@ -1,10 +1,12 @@
-# use of if else
-
-a=5
-b=10
-compare= a>b
-if compare==True:
-    print(" the compare is true  ") # then execute if
-else:
-    print(" the compare is false ") # then execute else
-    
+# # use of if else
+#
+# a=5
+# b=10
+# compare= a>b
+# if compare==True:
+#     print(" the compare is true  ") # then execute if
+# else:
+#     print(" the compare is false ") # then execute
+n= int(input("input your namber1:"))
+m= int(input("ibput your namber2:"))
+print(n +m )
