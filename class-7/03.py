4422dc173a4e7311f892044225a0152ff91f9668
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-08-03 22:26:54 +0600

    code

---
+++ class-7/03.py
@@ -0,0 +1,7 @@
+# List traversing
+fruits = ["Apple", "Mango", "Banana", "Jack Fruit", "Avegado"]
+
+for iterator in fruits:
+    print(f"{iterator} is a fruit.")
+
+# print("For ends ", iterator)
---
+++ class-7/05.py
@@ -0,0 +1,7 @@
+for i in range(1, 11, 2):
+    print(i)
+
+print("-----")
+
+for i in range(2, 11 , 2):
+    print(i)
