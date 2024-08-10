0ff0c958701eda704cd62ba43c4ca62a29b0cae0
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-08-10 15:52:25 +0600

    code

--- class-7/Homework/Medium/Medium_1.py
+++ class-7/Homework/Medium/Medium_1.py
@@ -9,3 +9,13 @@ for i in range(1,11):
     multiplication = n * i
     print(f"  {n} * {i} = {multiplication} ")
 
+
+
+#if you went 1 to n, all the programs are executed with together
+
+multiplication = 0
+for j in range(1,21):
+    print ( f" {j} th multiplication table :" )
+    for i in range ( 1 , 11 ) :
+        multiplication = j * i
+        print ( f"  {j} * {i} = {multiplication} " )
