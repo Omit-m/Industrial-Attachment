6991539fdac515a9d0c90acd8db89c25f766baf5
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-08-21 01:35:11 +0600

    code

--- class-7/Homework/Hard/Hard_1.py
+++ class-7/Homework/Hard/Hard_1.py
@@ -2,3 +2,14 @@
 [*] Check for Palindrome:
     Write a program to check if a string entered by the user is a palindrome using a for loop.
 """
+
+value  = input("Enter the String to check for palindrome : ")
+
+reverseval = value[::-1]
+
+print(reverseval)
+
+# if value == reverseval :
+#      print("Yes it is palindrome")
+# else:
+#      print("No it is Not a palindrome")
