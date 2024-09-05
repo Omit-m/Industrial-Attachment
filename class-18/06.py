6456fc4d4ce3838dd7a9c13f92bc9ca02d845c1f
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-09-05 11:44:35 +0600

    code

--- class-18/06.py
+++ class-18/06.py
@@ -3,6 +3,16 @@ class Person:
     def __init__(self):
         """Constructor"""
         print("hello")
+        """
+        print("hello", self)
+        
+        output : 
+            create reference by self : 
+            hello <__main__.Person object at 0x0000017D7317C1D0>
+            hello <__main__.Person object at 0x0000017D7317C250>
+            hello <__main__.Person object at 0x0000017D7317C290>
+            hello <__main__.Person object at 0x0000017D7317C2D0>
+            """
 
 
 # object creation
