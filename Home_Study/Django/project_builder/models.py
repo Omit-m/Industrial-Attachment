1f7597c7ae6ca26222f9fa92cc8d93091f0db0d7
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2024-10-04 01:04:24 +0600

    Code

--- Home_Study/Django/project_builder/models.py
+++ Home_Study/Django/project_builder/models.py
@@ -6,13 +6,18 @@ class project(models.Model):
 
 class location(models.Model):
     address = models.TextField()
-    models.OneToOneField('project',on_delete = models.SET_NULL,related_name='location' )
+    project = models.OneToOneField('project',
+    on_delete = models.SET_NULL,related_name='location' )
 
 
 
 class Task(models.Model):
     titel = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+    project = models.ForeignKey('project',
+    on_delete =models.CASCADE,related_name='task')
 
 class Tag(models.Model):    
     name= models.CharField(max_length=100)
+    projects =models.models.ManyToManyField('project',
+    related_name='tags')
